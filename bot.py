27c261bc13bfced121d8cba70b2ab3c0d6e91828
kirisame-sweet <86187634+kirisame-sweet@users.noreply.github.com>
2022-05-10 00:20:19 +0800

    1

---
+++ bot.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+MGRoid=commands.bot(command_prefix='/')
+
+@MGRoid.event
+async def on_ready():
+    print('>> bot is online <<')
+
+MGRoid.run(OTcyODcwNjU4MTMwMzI1NTk0.GJ8c8C.VC8c5AUwJcxg0a0Oh8PLhIhmMZcvnyRYLdFBto)
