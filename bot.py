a285a9accec1f5fd9b70ef4234829167b6093c50
kirisame-sweet <86187634+kirisame-sweet@users.noreply.github.com>
2022-05-10 21:50:38 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import discord
 from discord.ext import commands
 
-MGRoid=commands.bot(command_prefix='/')
+MGRoid=commands.Bot(command_prefix='/')
 
 @MGRoid.event
 async def on_ready():
     print('>> bot is online <<')
 
-MGRoid.run(OTcyODcwNjU4MTMwMzI1NTk0.GJ8c8C.VC8c5AUwJcxg0a0Oh8PLhIhmMZcvnyRYLdFBto)
+@MGRoid.command()
+async def fky(ctx)
+
+MGRoid.run('OTcyODcwNjU4MTMwMzI1NTk0.GJ8c8C.VC8c5AUwJcxg0a0Oh8PLhIhmMZcvnyRYLdFBto')
